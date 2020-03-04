6588469ff86d6ad4c1156a14033698ba6f161c86
andrew-nechai <panodora@mail.ru>
2020-03-04 18:10:08 +0300

    feat: add changes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  let otv = [];
-	x.forEach(t => {
-		t.forEach(k => {
-			otv.push(k);
-		})
+	if (matrix === undefined)
+		return [];
+
+	let otv = [], flag = 0;
+
+	matrix.forEach(t => {
+		if (flag == 1){
+			otv = otv.concat(t.reverse());
+			flag = 0;
+		} else {
+			otv = otv.concat(t);
+			flag = 1;
+		}
+
 	});
+
 	return otv;
 }
